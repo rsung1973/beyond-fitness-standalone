00ae9ecba7cc50b12008b77278863e23c5e4158b
ralph <rsung1973@hotmail.com>
2021-11-18 22:51:55 +0800

    [2021-11-18]:migrate to asp.net core

---
+++ Helper/PlugInHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CommonLib.PlugInAdapter;
+
+namespace CommonLib.Core.Helper
+{
+    public class PlugInHelper
+    {
+        private static PlugInHelper _instance;
+        private IPdfUtility _pdfUtility;
+        private ILogger _logger;
+        
+        static PlugInHelper() 
+        {
+            _instance = new PlugInHelper();
+        }
+
+        private PlugInHelper() 
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(Startup.Properties["IPdfUtilityImpl"]))
+                {
+                    Type type = Type.GetType(Startup.Properties["IPdfUtilityImpl"]);
+                    if (type.GetInterface("CommonLib.PlugInAdapter.IPdfUtility") != null)
+                    {
+                        _pdfUtility = (IPdfUtility)type.Assembly.CreateInstance(type.FullName);
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        public static IPdfUtility GetPdfUtility()
+        {
+            if (_instance._pdfUtility == null)
+            {
+                throw new Exception("未設定PDF輸出套件!!");
+            }
+            return _instance._pdfUtility;
+        }
+
+        public static ILogger GetLogger()
+        {
+            if (_instance._logger == null)
+            {
+                throw new Exception("未設定Logger!!");
+            }
+            return _instance._logger;
+        }
+
+    }
+}
