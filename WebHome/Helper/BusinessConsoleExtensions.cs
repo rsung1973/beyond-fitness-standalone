197be4dbbc7d8e6033734251316a19c4c29f2731
SATURN\ralph <rsung1973@hotmail.com>
2023-01-24 12:32:24 +0800

    [2023-01-24]:顯示收款金額

--- WebHome/Helper/BusinessConsoleExtensions.cs
+++ WebHome/Helper/BusinessConsoleExtensions.cs
@@ -23,7 +23,7 @@ using WebHome.Controllers;
 
 namespace WebHome.Helper
 {
-    public static class BusinessConsoleExtensions
+    public static partial class BusinessConsoleExtensions
     {
         public static MonthlyIndicator InitializeMonthlyIndicator(this GenericManager<BFDataContext> models, int year, int month, bool? forcedPrepare = null)
                 
@@ -238,42 +238,6 @@ namespace WebHome.Helper
 
             return item;
         }
-
-        public static readonly int?[] SessionScopeForAchievement = new int?[]
-                    {
-                        (int)Naming.LessonPriceStatus.一般課程,
-                        (int)Naming.LessonPriceStatus.已刪除,
-                        (int)Naming.LessonPriceStatus.團體學員課程,
-                    };
-
-        public static readonly int?[] SessionScopeForComleteLessonCount = new int?[]
-        {
-                    (int)Naming.LessonPriceStatus.一般課程,
-                    (int)Naming.LessonPriceStatus.已刪除,
-                    (int)Naming.LessonPriceStatus.點數兌換課程,
-                    (int)Naming.LessonPriceStatus.員工福利課程,
-                    (int)Naming.LessonPriceStatus.團體學員課程,
-        };
-
-        public static readonly int?[] SessionScopeForAveragePrice = new int?[]
-        {
-                    (int)Naming.LessonPriceStatus.一般課程,
-                    (int)Naming.LessonPriceStatus.已刪除,
-                    (int)Naming.LessonPriceStatus.點數兌換課程,
-                    (int)Naming.LessonPriceStatus.員工福利課程,
-                    (int)Naming.LessonPriceStatus.團體學員課程,
-                    (int)Naming.LessonPriceStatus.運動恢復課程,
-                    (int)Naming.LessonPriceStatus.運動防護課程,
-                    (int)Naming.LessonPriceStatus.營養課程,
-        };
-
-        public static readonly int?[] HSSessionScope = new int?[]
-        {
-                    (int)Naming.LessonPriceStatus.運動恢復課程,
-                    (int)Naming.LessonPriceStatus.運動防護課程,
-                    (int)Naming.LessonPriceStatus.營養課程,
-        };
-
 
         public static IQueryable<Payment> UpdateVoidShare(this GenericManager<BFDataContext> models, DateTime startDate, DateTime endExclusiveDate)
         {
