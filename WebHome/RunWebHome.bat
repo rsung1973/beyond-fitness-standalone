7bfacb0a263cfbe8a59bc065461cdbbd6a352971
SATURN\ralph <rsung1973@hotmail.com>
2024-05-26 13:05:04 +0800

    注意!!commit設定檔...

--- WebHome/RunWebHome.bat
+++ WebHome/RunWebHome.bat
@@ -3,4 +3,4 @@ SET ASPNETCORE_HOSTINGSTARTUPASSEMBLIES=Microsoft.AspNetCore.Mvc.Razor.RuntimeCo
 SET LAUNCHER_PATH=bin\Debug\net8.0\WebHome.exe
 rem "c:\Program Files\IIS Express\iisexpress.exe" /site:WebHome /config:C:\Project\Github\beyond-fitness\WebHome\.vs\WebHome\config\applicationhost.config /apppool:"WebHome AppPool"
 robocopy ..\WebDev\bin\Debug\net8.0 bin\Debug\net8.0 /S
-Start "WebHome" bin\Debug\net8.0\WebHome.exe --urls="http://*:5000"
+Start "WebHome" bin\Debug\net8.0\WebHome.exe --urls="http://*:5000;https://*:5443"
