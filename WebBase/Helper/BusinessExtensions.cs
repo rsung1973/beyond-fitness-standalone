832b9473e7ddb8247db2aecf0baf47f5b5942a0d
garfaild <garfaild@gmail.com>
2024-05-30 11:24:51 +0800

    新增交易管家並同時將CheckOTP移動到Module中

--- WebBase/Helper/BusinessExtensions.cs
+++ WebBase/Helper/BusinessExtensions.cs
@@ -15,6 +15,23 @@ namespace WebHome.Helper
             var pinCode = DateTime.Now.Ticks % 1000000;
             return $"{(char)('A' + (pinCode % 26))}{(char)('A' + (pinCode % 1000 % 26))}-{pinCode:000000}";
         }
+        
+        public static string CutString(this string input,char separator)
+        {
+            // 檢查輸入字串是否包含'-'字符
+            int index = input.IndexOf(separator);
 
+            // 如果包含'-'字符
+            if (index != -1)
+            {
+                // 回傳'-'之前的內容
+                return input.Substring(0, index);
+            }
+            else
+            {
+                // 如果不包含'-'字符，回傳整個字串
+                return input;
+            }
+        }
     }
 }
